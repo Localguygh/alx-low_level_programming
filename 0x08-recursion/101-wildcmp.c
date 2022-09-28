2c1b0f8eecd72924540e8cc1935f949d81f662c3
localguygh <enochquarcoo@yahoo.com>
2022-09-28 16:06:34 -0700

    oksa

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,29 +1,19 @@
-#include "main.h"	
-	/**
-	* prime2 - Makes possible to evaluate from 1 to n
-	* @a: same number as n
-	* @b: number that iterates from 1 to n
-	*
-	* Return: On success 1.
-	* On error, -1 is returned, and errno is set appropriately.
-	*/
-	int prime2(int a, int b)
-	{
-		if (a == b)
-			return (1);
-		else if (a % b == 0)
-			return (0);
-		return (prime2(a, b + 1));
-	}
-	/**
-	* is_prime_number - checks if a number is prime
-	* @n: Number Integer
-	*
-	* Return: On success 1.
-	* On error, -1 is returned, and errno is set appropriately.
-	*/
-	int is_prime_number(int n)
-	{
-		if (n <= 1)
-			return (0);
-		return (prime2(n, 2));
+#include "main.h"
+/**
+ * wildcmp - compares two strings.
+ * @s1: string 1.
+ * @s2: string 2. It can contains a * as a special character.
+ * Return: 1 if are identical, 0 if not.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
