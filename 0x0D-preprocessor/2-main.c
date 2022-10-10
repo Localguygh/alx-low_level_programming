22fd518bac024d66ec5b85ab2f89a366d3c86671
localguygh <enochquarcoo@yahoo.com>
2022-10-10 13:41:17 -0700

    soeeas

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,3 +1,11 @@
-#ifndef PI
-#define PI 3.14159265359
-#endif
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main file returns file name
+ * Return: returns 0 always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
