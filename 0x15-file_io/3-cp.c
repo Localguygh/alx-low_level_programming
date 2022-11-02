330cf14006d50d3199011013b28f53031f8f552a
localguygh <enochquarcoo@yahoo.com>
2022-11-02 04:21:16 -0700

    saw

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,13 +18,13 @@ int main(int ac, char **av)
 	fdFrum = open(av[1], O_RDONLY);
 	if (fdFrum == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE", av[1]);
 		exit(98);
 	}
 	fdToo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fdToo == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE", av[2]);
 		exit(99);
 	}
 	while ((readed = read(fdFrum, buff, 1024)) > 0)
@@ -32,23 +32,23 @@ int main(int ac, char **av)
 		wrote = write(fdToo, buff, readed);
 		if (wrote == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE", av[2]);
 			exit(99);
 		}
 	}
 	if (readed == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE", av[1]);
 		exit(98);
 	}
 	if (close(fdFrum) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdFrum);
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE", fdFrum);
 		exit(100);
 	}
 	if (close(fdToo) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdToo);
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE", fdToo);
 		exit(100);
 	}
 	return (0);
